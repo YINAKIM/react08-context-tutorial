df4827250a95c7f4ad694fc44b3736e6929d03e9
YINAKIM <yinaofficeg@gmail.com>
2022-02-19 22:25:39 +0900

    Provider를 사용하면 Context의 value값 변경하기

--- src/App.js
+++ src/App.js
@@ -1,11 +1,19 @@
 import './App.css';
 import ColorBox from "./components/ColorBox";
+import ColorContext from "./contexts/colors";
 
 function App() {
   return (
-      <div>
-        <ColorBox/>
-      </div>
+      <ColorContext.Provider value={{color: 'red'}}>
+          {/*
+            Provider를 사용하면 Context의 value를 변경할 수 있다.
+            - createContext에 넣어주는 defaultValue는 Provider를 사용하지 않았을 때만 사용된다.
+            - Provider를 사용하고 기본값보여준다고 value값을 "명시하지 않으면오류발생"
+           */}
+          <div>
+            <ColorBox/>
+          </div>
+      </ColorContext.Provider>
   );
 }
 
--- src/contexts/colors.js
+++ src/contexts/colors.js
@@ -1,5 +1,7 @@
 import {createContext} from "react";
 
 const ColorContext = createContext({color:'black'});
+                     // createContext에 넣어주는 defaultValue는 Provider를 사용하지 않았을 때만 사용된다.
+                     // 사용하는 컴포넌트에서 Provider를 사용하고 기본값보여준다고 값을 명시하지 않으면오류발생
 
 export default ColorContext;
